b80f3556fde2b37b45903bb6c7c1b228f8048248
andrewsamson01 <97703701+andrewsamson01@users.noreply.github.com>
2023-04-07 11:00:21 -0600

    Skeleton
    Added some skeleton functions that need to be made probably. Also wrote an equation relating homography matrices so we find position of bot relative to world (should be double checked). Imported brains as a library and defined it into transmitToArduino (still needs to be fixed so that it will actually transmit an argument)

--- Lab5/vision.py
+++ Lab5/vision.py
@@ -6,10 +6,40 @@ Aruco Detection
 
 import cv2 as cv
 import numpy as np
+from Brains import transmitToArduino
 # import argparse
 # import imutils
 # import sys
 
+selected_coordinates = (0,0) #globals because we need it
+def click_event(event, x, y, flags, params):
+    if event == cv.EVENT_LBUTTONDOWN:
+        #was clicked
+        selected_coordinates = (x, y)
+
+def moveBottoPoint(selectedPlace, botPose):
+    #my thoughtis its formatted selectedPlace = (x coordinate, ycoordinate)relative to world frme
+    #botPose = (x coordinate, y coordinate, w angle comp to world x axis) relative to world frame
+    #get vector of bot loc to selected place
+    bot_vector = ((selectedPlace(0) - botPose(0)), (selectedPlace(1) - botPose(1)))
+    #determine that angle
+    angle = np.arctan(bot_vector(1)/bot_vector(0))
+    
+    #compare that angle to w of bot
+
+    #turn accordingly so we face point (function)
+
+    #move straight
+    
+    z = 0
+
+def calculatePWMS():
+    
+    return 255, 255
+
+def getPoses():
+    x,y,z,w = 0,0,0,0
+    return x,y,z, w
 
 def main():
     scale = 1
@@ -44,6 +74,7 @@ def main():
             rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers(corners=corners, markerLength=MARKER_LENGTH, cameraMatrix=K, distCoeffs=None)
             # Get the pose of the first detected marker with respect to the camera.
             print(tvecs[0])
+            
             for i in range(0, len(rvecs)):
                 rvec_m_c = rvecs[i]  # This is a 1x3 rotation vector
                 tm_c = tvecs[i]  # This is a 1x3 translation vector
@@ -51,10 +82,18 @@ def main():
                 M_ext = np.block([[rotation_m_c[0], tm_c.T]])  # Creates homography from marker to camera
                 # frame = draw_pyramid(frame, M_ext, Switch[ids[0, :]], ids[0, :])  # Pass in values to draw the pyramid
                 cv.aruco.drawAxis(image=frame, cameraMatrix=K, distCoeffs=None, rvec=rvec_m_c, tvec=tm_c, length=MARKER_LENGTH / 2)
+            
+            #use M_ext to determine location of bot relative to world. 
+            P_B_B = np.block([[0,0,1]]).T #may need to be 0,0,0,1
+            P_b_w = np.linalg.inv(M_w_c) @ M_b_c @ P_B_B
         # Display the resulting frame
         cv.imshow('frame', frame)
+
         if cv.waitKey(1) == ord('q'):
             break
+        cv.setMouseCallback('frame', click_event)
+        left_pwm, right_pwm = 255, 255 #calculate pwms
+        transmitToArduino()
     # When everything done, release the capture
     cap.release()
 
